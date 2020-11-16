772b9c6f46398b1e0a8eb89a48ad0ed38e1f9f9d
Amph <amprubio@ucm.es>
2020-11-16 11:58:48 +0200

    practica 1 creada

---
+++ FP I/Practicas/Practica1/Practica1/Program.cs
@@ -0,0 +1,20 @@
+﻿/// Amparo Rubio Bellón
+using System;
+
+/// <summary>
+/// Practica 1 FP
+/// </summary>
+namespace Practica1
+{
+    class Program
+    {
+        //consts here
+
+        //end of consts
+        static void Main()
+        {
+        }
+        //methods ??? idk if we can use them
+
+    }
+}
