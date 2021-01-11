ed52b6b8cb9a2cb92b93dd0f2ce6d2aa46b70c63
Amph <amprubio@ucm.es>
2021-01-11 17:26:57 +0200

    Entrega final practica 2

--- FP I/Practicas/Practica2/Practica2/Program.cs
+++ FP I/Practicas/Practica2/Practica2/Program.cs
@@ -9,10 +9,13 @@ namespace Practica2
         static Random rnd = new Random(); // generador de números aleatorios
    
         //constantes
-        const int ANCHO = 30, ALTO = 18, MAX_BOMBAS = 10, COMBUSTIBLE = 15, ALTURA_EDIFICIO=5;
+        const int ANCHO = 30, ALTO = 18, MAX_BOMBAS = 10, COMBUSTIBLE = 15;
         const int RETARDO = 150;
         const bool DEBUG = false;
-        
+
+        const int ALTURA_EDIFICIO = 5; //altura del edificio en modo depuracion
+
+
         static void Main(string[] args)
         {
             int ancho=ANCHO; //ancho de la pantalla de juego , nº de edificios y de bombas
@@ -21,7 +24,7 @@ namespace Practica2
             int puntos; //puntuacion
             int combustible;//combustible
 
-            if (!DEBUG)
+            if (DEBUG)
             {
                 do
                 {
@@ -35,12 +38,13 @@ namespace Practica2
                       
             bool exit_ = false; //booleano de salida
             bool paused = false;//booleano de pausado
-            
+            bool col_ = false; //booleano de colision
+
             Inicializa(edificios, bombas, out avionX, out avionY, out puntos, out combustible);
             Renderiza(edificios, bombas, avionX, avionY, puntos, combustible);
 
             //bucle principal
-            while (!exit_ && FinPartida(edificios)){
+            while (!exit_ &&!col_ && FinPartida(edificios)){
                 
                 char c = leeInput();
             
@@ -69,14 +73,14 @@ namespace Practica2
                         //renderizado
                         Renderiza(edificios, bombas, avionX, avionY, puntos, combustible);
 
-                        exit_ = ColisionAvion(edificios, avionX, avionY); //o impacto avion-edificio
+                        col_ = ColisionAvion(edificios, avionX, avionY); //o impacto avion-edificio
                         System.Threading.Thread.Sleep(RETARDO);
 
                     }
                 }
             }
             
-            if (ColisionAvion(edificios, avionX, avionY)) //si la salida ha sido por un impacto dibujamos el punto de impacto
+            if (col_) //si la salida ha sido por un impacto dibujamos el punto de impacto
             {
                 Console.SetCursorPosition(2 * avionX, ALTO - avionY);
                 Console.BackgroundColor = ConsoleColor.DarkRed;
@@ -85,20 +89,26 @@ namespace Practica2
                 Console.Write("boom");
                 Console.SetCursorPosition(0, ALTO + 7);
             }
+            else if (!FinPartida(edificios))
+            {
+                Console.SetCursorPosition(0, ALTO + 7);
+                Console.WriteLine("Has eliminado todos los edificios");
+            }
 
         }
 
+        //inicializa el contenido de edificios y bombas, posiciona el avion y inicia el combustible
         static void Inicializa(int[] edificios, int[] bombas, out int avionX, out int avionY, out int puntos, out int combustible)
         {
             for (int i = 0; i < edificios.Length; i++)
             {
                 if (DEBUG)
                 {
-                    edificios[i] = ALTURA_EDIFICIO;
+                    edificios[i] = ALTURA_EDIFICIO; //altura fija dada por una constante
                 }
                 else
                 {
-                    edificios[i] = rnd.Next(1, ALTO - 3);
+                    edificios[i] = rnd.Next(1, ALTO - 3); //altura random 
                 }
                 bombas[i] = -1;
             }
@@ -108,24 +118,26 @@ namespace Practica2
             combustible = COMBUSTIBLE;
         }
 
+        //renderizado del juego, dibuja edificios, bombas si hay en pantalla, el avion y la informacion de puntos y combustible. 
+        //Si esta en depuracion añade informacion como el nº de bombas en pantalla, la pos del avion y la altura de cada edificio
         static void Renderiza(int[] edificios, int[] bombas, int avionX, int avionY, int puntos, int combustible)
         {
             Console.Clear();
             
-            ConsoleColor[] colors = (ConsoleColor[])ConsoleColor.GetValues(typeof(ConsoleColor));
-            int indColor = 1;
+            ConsoleColor[] colors = (ConsoleColor[])ConsoleColor.GetValues(typeof(ConsoleColor)); //array de colores
+            int indColor = 0; //color actual
          
             for (int i = 0; i < edificios.Length; i++)
             {
                 indColor++;
                 
-                if (indColor >= colors.Length) //esto quita el negro de la paleta
+                if (indColor >= colors.Length) //si supera la dimension del array volvemos al principio del array(1 para evitar el negro ya que no se veria)
                 {
                     indColor = 1;
                 }
-                else if (indColor == 4 ||indColor==11) //si es igual a rojo oscuro(bombas) o cyan (avion descartamos)
+                else if (indColor == 4 ||indColor==11) //si es igual a rojo oscuro(bombas) o cyan (avion descartamos ese color)
                 {
-                    indColor++;
+                    indColor=indColor++ % colors.Length;
                 }
                
                 Console.BackgroundColor = colors[indColor]; 
@@ -137,7 +149,7 @@ namespace Practica2
                     Console.Write("  ");
                 }
 
-                Console.BackgroundColor = colors[0];
+                Console.BackgroundColor = colors[0]; //si estamos en depuracion dibuja la altura de cada edificio
                 if (DEBUG)
                 {
                     Console.SetCursorPosition(2 * i, ALTO + 1);
@@ -152,15 +164,18 @@ namespace Practica2
                 }
             }
 
+            //dibujamos el avion
             Console.SetCursorPosition(2 * avionX, ALTO-avionY);
             Console.BackgroundColor = ConsoleColor.Cyan;
             Console.Write("<-");
 
             Console.BackgroundColor = ConsoleColor.Black;
-
+            
+            //dibujamos los puntos
             Console.SetCursorPosition(0, ALTO + 3);
             Console.Write("puntos " + puntos);
 
+            //dibujamos el combustible
             Console.SetCursorPosition(0, ALTO + 5);
             Console.Write("combustible " + combustible + " ");
 
@@ -179,6 +194,8 @@ namespace Practica2
             {
                 Console.Write(" ");
             }
+
+            //informacion adicional para la depuracion
             Console.BackgroundColor = ConsoleColor.Black;
             if (DEBUG) { 
              
@@ -188,7 +205,8 @@ namespace Practica2
             
             Console.WriteLine();
         }
-
+        
+        //devuelve un char en funcion del input
         static char leeInput()
         {
             char d = ' ';
@@ -213,6 +231,7 @@ namespace Practica2
 
         }
 
+        //devuelve true si aún quedan edificios en pie
         static bool FinPartida(int[] edificios)
         {
             int cont = 0;
@@ -224,6 +243,7 @@ namespace Practica2
             return cont < edificios.Length;
         }
 
+        //Lanza una bomba en la posicion avionX a la altura avionY si es posible (si no ha llegado al maximo de bombas en pantalla y no hay una bomba en avionX)
         static void LanzamientoBomba(int[] bombas, int avionX, int avionY)
         {
             if (CuentaBombas(bombas) < MAX_BOMBAS && bombas[avionX] < 0)
@@ -231,6 +251,8 @@ namespace Practica2
                 bombas[avionX] = avionY;
             }
         }
+        
+        //devuelve el numero de bombas en pantalla
         static int CuentaBombas(int[] bombas)
         {
             int cont = 0;
@@ -241,6 +263,8 @@ namespace Practica2
             }
             return cont;
         }
+        
+        //mueve las bombas que esten en pantalla
         static void MueveBombas(int[] bombas)
         {
             for (int i = 0; i < bombas.Length; i++)
@@ -253,6 +277,7 @@ namespace Practica2
 
         }
 
+        //mueve el avion acorde a su input y resta combustible cuando es necesario
         static void MueveAvion(char c, ref int avionX, ref int avionY, int ancho, ref int combustible)
         {
             if (combustible > 0)
@@ -260,7 +285,7 @@ namespace Practica2
                 switch (c)
                 {
                     case 'a':
-                        avionX -= 2;
+                        avionX -= 1;
                         combustible--;
                         break;
                     case 'w':
@@ -291,13 +316,13 @@ namespace Practica2
             }
         }
 
+        //Comprueba la colision de las bombas y añade puntos en funcion de ello
         static void ColisionBombas(int[] edificios, int[] bombas, ref int puntos)
         {
             for (int i = 0; i < edificios.Length; i++)
             {
                 if (bombas[i]>=0 && bombas[i]<= edificios[i])
                 {
-
                     Impacto(edificios, i, ref puntos);
                     edificios[i]--;
                     puntos = puntos + 1;
@@ -307,14 +332,14 @@ namespace Practica2
             }
         }
 
+        // recorre el array de edificios desde un punto i por la izq y derecha y resta la altura y añade puntos en funcion de si tienen la misma altura que edificios[i]
         static void Impacto(int [] edificios, int i, ref int puntos)
         {
 
             int indIzq = i - 1;
             int indDer = i + 1;
 
-
-            //mover esto a metodo impacto
+            //recorrido por la izquierda
             while (indIzq >= 0 && edificios[i] == edificios[indIzq])
             {
                 edificios[indIzq]--;
@@ -322,6 +347,7 @@ namespace Practica2
                 puntos += 1;
             }
 
+            //recorrido por la derecha
             while (indDer < edificios.Length && edificios[i] == edificios[indDer])
             {
                 edificios[indDer]--;
@@ -330,6 +356,7 @@ namespace Practica2
             }
         }
 
+        //devuelve true si el avion ha impactado con un edificio
         static bool ColisionAvion(int []edificios, int avionX, int avionY)
         {
             return edificios[avionX] > avionY;
