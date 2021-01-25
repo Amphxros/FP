33e49d326fd8fb822d7ec2a69db838a39a3f2eaf
Amph <amprubio@ucm.es>
2021-01-25 17:20:46 +0200

    examen 2017 parte 1

---
+++ FP I/Examenes/ExamenFeb2017/ExamenFeb2017/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+namespace ExamenFeb2017
+{
+    class Program
+    {
+        static Random rnd = new Random();
+        static void Main(string[] args)
+        {
+            const int MAX_FALLOS = 10;
+
+            string pal="CELESTINA";
+            bool[] descubiertas = new bool[pal.Length];
+            int fallos = 0;
+
+
+            for(int i = 0; i < descubiertas.Length; i++)
+            {
+                descubiertas[i] = rnd.Next(-1, 2) == 0;
+            }
+            Muestra(pal, descubiertas, 0);
+
+            while(!PalabraAcertada(descubiertas)&& fallos < MAX_FALLOS)
+            {
+                char c = LeeLetra();
+                DescubreLetras(pal, descubiertas, c, false);
+                Muestra(pal, descubiertas, fallos);
+            }
+        }
+
+        static void Muestra(string pal, bool [] descubiertas, int fallos)
+        {
+            Console.Clear();
+            for(int i = 0; i < descubiertas.Length; i++)
+            {
+                Console.SetCursorPosition(2 * (i + 3), 4);
+                if (descubiertas[i])
+                {
+                    Console.Write(pal[i]);
+                }
+                else
+                {
+                    Console.Write("- ");
+                }
+            }
+            Console.WriteLine();
+            Console.SetCursorPosition(5, 10);
+            Console.Write("Escribe letra: ");
+        }
+        static bool PalabraAcertada(bool [] descubiertas)
+        {
+            int i = 0;
+            while (i < descubiertas.Length && descubiertas[i])
+                i++;
+            return i>=descubiertas.Length;
+        }
+
+        static char LeeLetra()
+        {
+            char c= char.Parse(Console.ReadLine());
+            c = char.ToUpper(c);
+            
+            return c;
+        }
+        static void DescubreLetras(string pal, bool[] descubiertas, char c, bool acierto)
+        {
+            for(int i = 0; i < descubiertas.Length; i++)
+            {
+                if(pal[i]==c && !descubiertas[i])
+                {
+                    descubiertas[i] = true;
+                    acierto = true;
+                }
+            }
+
+          
+        }
+    }
+}
