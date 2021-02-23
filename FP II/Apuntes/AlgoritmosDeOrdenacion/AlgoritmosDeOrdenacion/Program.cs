268855d67689cd7a065a6d93c0211b1a0e8a0939
Amph <amprubio@ucm.es>
2021-02-23 11:05:58 +0100

    Algoritmos de ordenacion

---
+++ FP II/Apuntes/AlgoritmosDeOrdenacion/AlgoritmosDeOrdenacion/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmosDeOrdenacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static void burbuja(int[] v)
+        {
+            int n = v.Length;
+            // damos n vueltas, en cada una v[i] queda en su sitio
+            for (int i = 0; i < n; i++)
+            {
+                // inv: v [0.. i−1] ordenado (en su posición nal )
+                // recorremos con j=n−1 hasta i+1 comparando v[j−1] y v[j]
+                for (int j = n - 1; j < 0; j--)
+                {
+                    if (v[j - 1] > v[j])
+                    {
+                        swap(ref v[j - 1], ref v[j]);
+                    }
+                }
+            }
+        }
+        static void insercion(int[] v)
+        {
+            int n = v.Length; // número de eltos del vector
+                              // v[0] ya está ordenado, para cada uno desde 1 hasta n:
+            for (int i = 1; i < n; i++)
+            { // inv: v [0.. i−1] está ordenado
+              // insertamos v[i ] ordenadamente en el subvector v[0..i−1]
+                int tmp = v[i]; // guardamos v[i]
+                int j = i-1;
+                // desplazamos eltos a la dcha abriendo hueco para v[i]
+                while ((j >= 0) && (v[j] > tmp))
+                {
+                    v[j + 1] = v[j];
+                    j--;
+                }
+                v[j + 1] = tmp;
+            }
+        }
+       
+        static void seleccion(int[] v)
+        {
+            int n = v.Length; // num de eltos del vector
+                              // en cada posición i=0..n−1 ponemos el menor de v[i..n−1]
+                              // vamos hasta i=n−2 porque al nal v[n−1] ya está en su sitio
+            for (int i = 0; i < n-1; i++) {
+                // inv: el subarray 0.. i−1 está ordenado
+                // buscamos el menor en v[i..n)
+                int min = i;
+                for (int j = i + 1; j < n; j++)
+                    if (v[j] < v[min]) // con menor estricto es estable
+                        min = j;
+                // ponemos el menor en v[i] y v[i ] en la pos del menor
+                swap(ref v[i], ref v[min]);
+            }
+        }
+
+        static void swap(ref int x, ref int y)
+        {
+            int tmp = x;
+            x = y;
+            y = tmp;
+        }
+
+    }
+}
---
+++ FP II/Foro/StructsPolinomicos/StructsPolinomicos/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructsPolinomicos
+{
+    public struct Monomio
+    {
+        public int exp_;
+        public double base_;
+    }
+    public struct Polinomio
+    {
+        public Monomio[] mon;
+        public int tam;
+    }
+
+    class Program
+    {
+        const int N =3;
+        static void Main(string[] args)
+        {
+            Polinomio p;
+            leePolinomio(out p);
+            MuestraPol(p);
+
+        }
+
+        static void Inserta(ref Polinomio p)
+        {
+            Monomio m;
+            leeMonomio(out m);
+            int i = 0;
+            while(i<p.tam && p.mon[i].exp_ != m.exp_)
+            {
+                i++;
+            }
+
+        }
+
+        static void leePolinomio(out Polinomio p)
+        {
+            p.mon = new Monomio[N];
+            Console.Write("Número de monomios: ");
+            p.tam = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduce monomios");
+            for (int i = 0; i < p.tam; i++)
+                leeMonomio(out p.mon[i]);
+        }
+
+        static void leeMonomio(out Monomio m)
+        {
+            Console.Write("Coeficiente: ");
+            m.base_ = double.Parse(Console.ReadLine());
+            Console.Write("Exponente: ");
+            m.exp_ = int.Parse(Console.ReadLine());
+        }
+        static void MuestraPol(Polinomio pol)
+        {
+            for(int i=0;i<pol.tam; i++)
+            {
+                Console.Write(pol.mon[i].base_ + "x^" + pol.mon[i].exp_+ " ");
+            }
+        }
+        
+    }
+}
