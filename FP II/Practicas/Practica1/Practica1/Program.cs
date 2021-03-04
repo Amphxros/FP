1608efa0393323cbea28bdcada553ef1ab3c14f2
Amph <amprubio@ucm.es>
2021-03-04 10:44:03 +0100

    Practica1 parte 1

---
+++ FP II/Practicas/Practica1/Practica1/Program.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.IO;
+
+namespace Practica1
+{
+    struct Coor { public int fil, col; }; // coordenadas fila y columna en el tablero
+    enum TipoCasilla { Muro, Libre, Destino }; // 3 tipos de casillas en el tablero
+    struct Casilla
+    {
+        public TipoCasilla tipo; // información fija de la casilla (muro, libre o destino)
+        public bool caja; // información variable: si tiene o no caja
+    }
+    struct Tablero
+    { // tipo tablero
+        public Casilla[,] cas; // matriz de casillas
+        public Coor jug; // posición del jugador
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tablero t = LeeNivel("levels", 1);
+            Dibuja(t, 0);
+        }
+        static Tablero LeeNivel(string file,int n)
+        {
+            Tablero tab= new Tablero();
+            StreamReader read_ = new StreamReader(file);
+            if (read_ != null)
+            {
+                int fils = 0;
+                int cols=0;
+                bool encontrado = false, flag=false; //booleano de encontrar el nivel y bool para encontrar el blanco de linea
+                
+                string[] tmp = new string[15]; //array temporal
+
+                while(!read_.EndOfStream &&  fils<tmp.Length && !flag)
+                {
+                    if (!encontrado) {
+                        string line = read_.ReadLine();
+                        string[] pals = line.Split(' ');
+                        if (pals.Length==2 && pals[0]=="Level" && int.Parse(pals[1].ToString())==n)
+                        {
+                            encontrado = true;
+                        } 
+                    }
+                    else
+                    {
+                        tmp[fils] = read_.ReadLine();
+
+                        string[] pals = tmp[fils].Split(' ');
+                        if (pals[0] == "" || pals[0]=="Level")
+                        {
+                            flag = true;
+                        }
+                       else if (cols < tmp[fils].Length)
+                        {
+                            cols =tmp[fils].Length; 
+                        }
+
+                        Console.WriteLine(tmp[fils]);
+                        fils++;
+                    }
+                }
+
+                tab.cas = new Casilla[fils, cols];
+
+                for(int i=0; i < fils; i++)
+                {
+                    for(int j = 0; j < cols; j++)
+                    {
+                        tab.cas[i, j].tipo = TipoCasilla.Muro;
+                    }
+
+                }
+
+                for (int i = 0; i < fils; i++)
+                {
+                   
+                    for (int j = 0; j < tmp[i].Length; j++)
+                    {
+                        switch (tmp[i][j])
+                        {
+                            case '.':
+                                tab.cas[i, j].tipo = TipoCasilla.Destino;
+                                tab.cas[i, j].caja = false;
+                                break;
+
+                            case ' ':
+                                tab.cas[i, j].tipo = TipoCasilla.Libre;
+                                tab.cas[i, j].caja = false;
+                                break;
+
+                            case '$':
+                                tab.cas[i, j].tipo = TipoCasilla.Libre;
+                                tab.cas[i, j].caja = true;
+                                break;
+                            case '@':
+                                tab.cas[i, j].tipo = TipoCasilla.Libre;
+                                tab.jug.fil = i;
+                                tab.jug.col = j;
+                                break;
+                            default:
+                                tab.cas[i, j].tipo = TipoCasilla.Muro;
+                                tab.cas[i, j].caja = false;
+                                break;
+                        }
+                    }
+
+                }
+              
+
+            }
+
+            
+
+            return tab;
+
+        }
+        static void Dibuja(Tablero tab, int mov)
+        {
+            Console.Clear();
+            for(int i = 0; i < tab.cas.GetLength(0); i++)
+            {
+                for (int j = 0; j < tab.cas.GetLength(1); j++)
+                {
+                    Console.SetCursorPosition(2 * i, j);
+                      switch (tab.cas[i, j].tipo)
+                      {
+                          case TipoCasilla.Muro:
+                              Console.BackgroundColor = ConsoleColor.DarkCyan;
+                              Console.Write("  ");
+
+                              break;
+                          case TipoCasilla.Libre:
+                            if (tab.cas[i, j].caja)
+                            {
+                                Console.BackgroundColor = ConsoleColor.Red;
+                                Console.Write("[]");
+                            }
+                            else
+                            {
+                                Console.BackgroundColor = ConsoleColor.Black;
+                                 Console.Write("  ");
+                            }
+                            break;
+                          case TipoCasilla.Destino:
+
+                              if (tab.cas[i, j].caja)
+                              {
+                              Console.BackgroundColor = ConsoleColor.Yellow;
+                              }
+                              else
+                              {
+                                Console.BackgroundColor = ConsoleColor.Black;
+
+                              }
+                                Console.Write("()");
+                              break;
+                
+                      }
+
+                    Console.BackgroundColor = ConsoleColor.Black;
+                }
+
+            }
+
+            Console.SetCursorPosition(2 * tab.jug.fil, tab.jug.col);
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.Write("ºº");
+            
+            Console.SetCursorPosition(20,20);
+        }
+
+        static bool Siguiente(Coor pos, char dir, Tablero tab, Coor sig)
+        {
+            return false;
+        }
+        static char Mueve(Tablero tab, char dir)
+        {
+            return 'a';
+        }
+        static void ProcesaInput(Tablero tab, char dir, string movs)
+        {
+
+        }
+
+        static bool Terminado(Tablero tab)
+        {
+            return false;
+        }
+
+    }
+}
